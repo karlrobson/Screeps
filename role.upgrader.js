efd25b89af45ad678b3897b55746082179a86823
Karl Robson <karlalexrobson@gmail.com>
2016-06-23 14:29:37 +1200

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -4,10 +4,17 @@ var roleUpgrader = {
     run: function(creep) {
         
         if(creep.carry.energy == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            var loc = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
+                }});
+            if(loc[0].transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(loc[0]);
             }
+            //var sources = creep.room.find(FIND_SOURCES);
+            //if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                //creep.moveTo(sources[0]);
+            //}
         }
         
         else {
