b3de55d782bf8f1fb07bc9c83484bfddada6558c
Karl Robson <karlalexrobson@gmail.com>
2016-06-25 16:39:47 +1200

    Removed commented out code

--- role.upgrader.js
+++ role.upgrader.js
@@ -10,10 +10,7 @@ var roleUpgrader = {
             if(loc[0].transferEnergy(creep) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(loc[0]);
             }
-            //var sources = creep.room.find(FIND_SOURCES);
-            //if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                //creep.moveTo(sources[0]);
-            //}
+            
         }
         //Upgrades the controller
         else {
